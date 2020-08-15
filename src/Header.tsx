6a2779f5d22a34f800a85cef429c2885efdb337d
平野正樹 <contiki9@gmail.com>
2020-08-15 18:27:51 +0900

    👍Fix(header) ヘッダ周りのデザイン微調整

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,38 +1,54 @@
-import React, { Component } from 'react'
-import { Link } from 'gatsby'
-import { Menu, Row } from 'antd'
-import { GithubOutlined, TwitterOutlined } from '@ant-design/icons'
+import React, { Component } from 'react';
+import { Link } from 'gatsby';
+import { Menu, Row, Col } from 'antd';
+import { GithubOutlined, TwitterOutlined } from '@ant-design/icons';
 
 interface Props {
-  siteTitle: string
+  siteTitle: string;
 }
 
 export class Header extends Component<Props> {
   render() {
-    const { siteTitle } = this.props
+    const { siteTitle } = this.props;
     return (
       <>
-        <Menu mode="horizontal">
-          <Menu.Item>
-            <Link to="/">{siteTitle}</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <a
-              href="https://github.com/jannikbuschke/gatsby-antd-docs"
-              target="_blank"
-            >
-              <GithubOutlined />
-              GitHub
-            </a>
-          </Menu.Item>
-          <Menu.Item>
-            <a href="https://twitter.com/jannikbuschke" target="_blank">
-              <TwitterOutlined />
-              Twitter
-            </a>
-          </Menu.Item>
-        </Menu>
+        <Row
+          justify="center"
+          align="middle"
+          style={{
+            paddingLeft: 16,
+            paddingRight: 16,
+            borderBottom: '1px solid #f0f0f0',
+          }}
+        >
+          <Col>
+            <p style={{ fontSize: 16, marginBottom: 0 }}>
+              <Link style={{ color: '#333' }} to="/">
+                {siteTitle}
+              </Link>
+            </p>
+          </Col>
+          <Col style={{ marginLeft: 'auto' }}>
+            <Menu mode="horizontal">
+              <Menu.Item>
+                <a
+                  href="https://github.com/jannikbuschke/gatsby-antd-docs"
+                  target="_blank"
+                >
+                  <GithubOutlined />
+                  GitHub
+                </a>
+              </Menu.Item>
+              <Menu.Item>
+                <a href="https://twitter.com/jannikbuschke" target="_blank">
+                  <TwitterOutlined />
+                  Twitter
+                </a>
+              </Menu.Item>
+            </Menu>
+          </Col>
+        </Row>
       </>
-    )
+    );
   }
 }
